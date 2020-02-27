b3f8738ddbe70db2ecf04b42722b5f4f5544c160
mitrovickristina <mitrovickristina.dev@gmail.com>
2020-02-27 22:23:36 +0100

    novogradnja

---
+++ js/index.js
@@ -0,0 +1,11 @@
+
+        $(document).ready(function(){
+            $('.slider').slick({
+              dots: true,
+              arrows: true,
+              autoplay: true,
+              autoplaySpeed: 5000
+    
+            });
+          });
+
